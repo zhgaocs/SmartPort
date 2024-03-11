c7af71d62938162c78fc1048d3adc62a344b5a9d
zhgaocs <93659392+zhgaocs@users.noreply.github.com>
2024-03-11 15:32:47 +0800

    Update harbor.h

--- harbor.h
+++ harbor.h
@@ -4,9 +4,9 @@
 struct Robot
 {
     int x, y;
-    int goods;
-    int status;
-    int mbx, mby;
+    int has_item; /* 0 - no items, 1 - has items */
+    int status;   /* 0 - recover, 1 - running */
+    int dst_x, dst_y;
 
     Robot() = default;
     Robot(int startX, int startY) : x(startX), y(startY) {}
@@ -14,9 +14,10 @@ struct Robot
 
 struct Berth
 {
-    int x, y;
-    int transport_time;
-    int loading_speed;
+    int id;             /* 0 <= id < BERTH_NUM */
+    int x, y;           /* upper left corner */
+    int transport_time; /* 1 <=  transport_time <= 1000, transport time to virtual point*/
+    int loading_speed;  /* 1 <= loading_speed <= 5, the number of items that can be loaded per frame */
 
     Berth() = default;
     Berth(int x, int y, int transport_time, int loading_speed)
@@ -25,9 +26,16 @@ struct Berth
 
 struct Boat
 {
-    int num;
-    int pos;
-    int status;
+    int id;              /* 0 <= id < BOAT_NUM */
+    int capacity;        /* 1 <= capacity <= 1000 */
+    int target_berth_id; /* -1 - virtual point */
+    int status;          /* 0 - moving, 1 - running, 2 - waiting */
+};
+
+struct Item /* goods */
+{
+    int x, y;
+    int value; /* value <= 1000 */
 };
 
 #endif
