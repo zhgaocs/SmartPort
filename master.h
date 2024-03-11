48e299056f2b1f392f709e741ae7c4ea21ed8d57
Zhihui Gao <2049878697@qq.com>
2024-03-12 00:06:27 +0800

    Update master.h
    Delete #include "macros.h", and use constexpr instead of macros

--- master.h
+++ master.h
@@ -10,7 +10,6 @@
 #include <vector>
 
 #include "harbor.h"
-#include "macros.h"
 #include "utils.h"
 
 namespace std
@@ -41,19 +40,27 @@ private:
     findPath(int src_x, int src_y, int dst_x, int dst_y);
 
 private:
+    static constexpr int N = 200;
+    static constexpr int ROBOT_NUM = 10;
+    static constexpr int BERTH_NUM = 10;
+    static constexpr int BOAT_NUM = 10;
+
+private:
     char map[N][N];
     Robot robots[ROBOT_NUM];
     Berth berths[BERTH_NUM];
     Boat boats[BOAT_NUM];
     std::unordered_set<Item> items;
 
-private: 
-         /**
-          * these vars used in findPath 
-          * for map:
-          * the downward direction is the positive x direction,
-          * the right direction is the positive y direction.
-          */
+private:
+    /**
+     * these vars used in findPath
+     * for map:
+     * the downward direction is the positive x direction,
+     * the right direction is the positive y direction.
+     */
+    static constexpr char PATHWAY_SYMBOL = '.';
+    static constexpr int NUM_OF_DIRECTIONS = 4;
     static constexpr int DX[NUM_OF_DIRECTIONS] = {-1, 1, 0, 0};
     static constexpr int DY[NUM_OF_DIRECTIONS] = {0, 0, -1, 1};
     static constexpr int COST[NUM_OF_DIRECTIONS] = {1, 1, 1, 1};
