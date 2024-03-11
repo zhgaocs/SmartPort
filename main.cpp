90b6cd08c268559761804a01aa80f97c467d5e91
zhgaocs <93659392+zhgaocs@users.noreply.github.com>
2024-03-11 15:35:19 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,95 +1,76 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include "harbor.h"
+#include "macros.h"
 
-const int n = 200;
-const int robot_num = 10;
-const int berth_num = 10;
-const int N = 210;
+char ok_str[3] = "OK";
 
-struct Robot
-{
-    int x, y, goods;
-    int status;
-    int mbx, mby;
-    Robot() {}
-    Robot(int startX, int startY) {
-        x = startX;
-        y = startY;
-    }
-}robot[robot_num + 10];
+char map[N][N];
+Robot robot[ROBOT_NUM];
+Berth berth[BERTH_NUM];
+Boat boat[BOAT_NUM];
 
-struct Berth
-{
-    int x;
-    int y;
-    int transport_time;
-    int loading_speed;
-    Berth(){}
-    Berth(int x, int y, int transport_time, int loading_speed) {
-        this -> x = x;
-        this -> y = y;
-        this -> transport_time = transport_time;
-        this -> loading_speed = loading_speed;
-    }
-}berth[berth_num + 10];
-
-struct Boat
-{
-    int num, pos, status;
-}boat[10];
-
-int money, boat_capacity, id;
-char ch[N][N];
-int gds[N][N];
 void Init()
 {
-    for(int i = 1; i <= n; i ++)
-        scanf("%s", ch[i] + 1);
-    for(int i = 0; i < berth_num; i ++)
+    /* map */
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < N; ++j)
+            std::cin >> map[i][j];
+
+    /* berth */
+    for (int i = 0; i < BERTH_NUM; ++i)
     {
         int id;
-        scanf("%d", &id);
-        scanf("%d%d%d%d", &berth[id].x, &berth[id].y, &berth[id].transport_time, &berth[id].loading_speed);
+        std::cin >> id >> berth[id].x >> berth[id].y >> berth[id].transport_time >> berth[id].loading_speed;
     }
-    scanf("%d", &boat_capacity);
-    char okk[100];
-    scanf("%s", okk);
-    printf("OK\n");
-    fflush(stdout);
+
+    /* boat capacity */
+    int boat_capacity;
+    std::cin >> boat_capacity;
+
+    for (int i = 0; i < BOAT_NUM; ++i)
+        boat[i].capacity = boat_capacity;
+
+    /* OK */
+    std::cin >> ok_str;
+    std::cout << "OK" << std::flush;
 }
 
-int Input()
+void Input()
 {
-    scanf("%d%d", &id, &money);
-    int num;
-    scanf("%d", &num);
-    for(int i = 1; i <= num; i ++)
+    int frame_num, current_money; /* frame_id increase from 1 */
+    std::cin >> frame_num >> current_money;
+
+    /* new item */
+    int K;
+    std::cin >> K;
+    for (int i = 0; i < K; ++i)
     {
-        int x, y, val;
-        scanf("%d%d%d", &x, &y, &val);
+        Item item;
+        std::cin >> item.x >> item.y >> item.value;
     }
-    for(int i = 0; i < robot_num; i ++)
-    {
-        int sts;
-        scanf("%d%d%d%d", &robot[i].goods, &robot[i].x, &robot[i].y, &sts);
-    }
-    for(int i = 0; i < 5; i ++)
-        scanf("%d%d\n", &boat[i].status, &boat[i].pos);
-    char okk[100];
-    scanf("%s", okk);
-    return id;
+
+    /* robot */
+    for (int i = 0; i < ROBOT_NUM; ++i)
+        std::cin >> robot[i].has_item >> robot[i].x >> robot[i].y >> robot[i].status;
+
+    /* boat */
+    for (int i = 0; i < BOAT_NUM; ++i)
+        std::cin >> boat[i].status >> boat[i].target_berth_id;
+
+    /* OK */
+    std::cin >> ok_str;
 }
 
 int main()
 {
     Init();
-    for(int zhen = 1; zhen <= 15000; zhen ++)
+    for (int frame_cnt = 0; frame_cnt < 15000; ++frame_cnt)
     {
-        int id = Input();
-        for(int i = 0; i < robot_num; i ++)
-            printf("move %d %d\n", i, rand() % 4);
-        puts("OK");
-        fflush(stdout);
+        Input();
+        for (int i = 0; i < ROBOT_NUM; ++i)
+            ; // std::cout << "commands";
+
+        std::cout << "OK" << std::flush;
     }
 
     return 0;
