63fd7fd33374407c250558182b60246e840caaaf
Zhihui Gao <2049878697@qq.com>
2024-03-14 23:35:52 +0800

    Modify init() and assignTasks()

--- master.cpp
+++ master.cpp
@@ -22,8 +22,8 @@ void Master::init()
 
     /* boat capacity */
     std::cin >> boat_capacity;
-    for (int i = 0; i < BOAT_NUM; ++i)
-        boats[i].capacity = boat_capacity;
+    std::for_each(boats, boats + BOAT_NUM, [](Boat &b)
+                  { b.target_berth_id = -1, boats[i].status = 1, boats[i].capacity = boat_capacity; });
 
     /* OK */
     std::cin >> ok_cstr;
@@ -86,62 +86,83 @@ void Master::assignTasks()
     std::priority_queue<IMPair, std::vector<IMPair>, CompareIMPair> queue;
     std::vector<std::pair<int, int>> path, shortest_path;
 
-    for (int i = 0; i < ROBOT_NUM; ++i)
+    /* Robot */
+    for (int i = 0; i < ROBOT_NUM && robots[i].status; ++i)
     {
-        if (robots[i].status)
+        if (!has_tasks[i]) // no tasks
         {
-            if (!has_tasks[i]) // no tasks
+            if (!robots[i].has_item) // no items - select item
             {
-                if (!robots[i].has_item) // no items - select item
+                for (int j = 0; j < items.size(); ++j)
                 {
-                    for (int j = 0; j < items.size(); ++j)
+                    IMPair pair = std::make_pair(j, Manhattan(robots[i].x, robots[i].y, items[j].x, items[j].y));
+
+                    if (queue.size() < nearest_num)
+                        queue.push(pair);
+                    else if (pair.second < queue.top().second)
                     {
-                        IMPair pair = std::make_pair(j, Manhattan(robots[i].x, robots[i].y, items[j].x, items[j].y));
-
-                        if (queue.size() < nearest_num)
-                            queue.push(pair);
-                        else if (pair.second < queue.top().second)
-                        {
-                            queue.pop();
-                            queue.push(pair);
-                        }
-                    }
-
-                    int closest_idx, current_dist, min_dist = INT_MAX;
-                    IMPair pair;
-
-                    while (!queue.empty())
-                    {
-                        pair = queue.top();
-
-                        path = findPath(robots[i].x, robots[i].y, items[pair.first].x, items[pair.first].y);
-
-                        if ((current_dist = path.size()) && current_dist < min_dist && current_dist - 1 < items[pair.first].life_span) // accessible
-                        {
-                            closest_idx = pair.first;
-                            min_dist = current_dist;
-                            shortest_path = path;
-                        }
                         queue.pop();
-                    }
-
-                    if (INT_MAX != min_dist) // accessible
-                    {
-                        has_tasks[i] = true;
-                        paths[i] = path2Directions(shortest_path);
-                        items.erase(items.begin() + closest_idx);
+                        queue.push(pair);
                     }
                 }
-            }
-            else // has tasks
-            {
-                if (!robots[i].has_item) // no items - on the way
-                    ;                    // nothing to do
-                else                     // has item, select berth
+
+                int closest_idx, current_dist, min_dist = INT_MAX;
+                IMPair pair;
+
+                while (!queue.empty())
                 {
+                    pair = queue.top();
+
+                    path = findPath(robots[i].x, robots[i].y, items[pair.first].x, items[pair.first].y);
+
+                    if ((current_dist = path.size()) && current_dist < min_dist && current_dist - 1 < items[pair.first].life_span) // accessible
+                    {
+                        closest_idx = pair.first;
+                        min_dist = current_dist;
+                        shortest_path = path;
+                    }
+                    queue.pop();
+                }
+
+                if (INT_MAX != min_dist) // accessible
+                {
+                    has_tasks[i] = true;
+                    paths[i] = path2Directions(shortest_path);
+                    items.erase(items.begin() + closest_idx);
                 }
             }
         }
+        else if (robots[i].has_item && paths[i].empty()) // has tasks && has item, select berth
+        {
+            int current_dist, min_dist = INT_MAX;
+            std::vector<std::pair<int, int>> vec_p, path, shortest_path;
+
+            for (int j = 0; j < BERTH_NUM; ++j)
+            {
+                vec_p = findBerthPoint(berths[j].x, berths[j].y);
+
+                for (int k = 0; k < vec_p.size(); ++k) // 0 <= vecp.size() <= 12
+                {
+                    path = findPath(robots[i].x, robots[i].y, vec_p[k].first, vec_p[k].second);
+
+                    if ((current_dist = path.size()) && current_dist < min_dist)
+                    {
+                        min_dist = current_dist;
+                        shortest_path = path;
+                    }
+                }
+            }
+
+            if (min_dist != INT_MAX)
+                paths[i] = path2Directions(shortest_path);
+            else // robots[i] cannot find berth
+                ;
+        }
+    }
+
+    /* Ship */
+    for (int i = 0; i < BOAT_NUM && boats[i].status; ++i)
+    {
     }
 }
 
@@ -161,10 +182,9 @@ void Master::controlOutput() const
     std::cout << "OK" << std::flush;
 }
 
-std::vector<std::pair<int, int>> Master::findPath(int src_x, int src_y, int dst_x, int dst_y)
+std::vector<std::pair<int, int>> Master::findPath(int src_x, int src_y, int dst_x, int dst_y) const
 {
     /* ------------------------------------------------------------------------------------------- */
-    static constexpr char PATHWAY_SYMBOL = '.';
     static constexpr int NUM_OF_DIRECTIONS = 4;
     static constexpr int DX[NUM_OF_DIRECTIONS] = {-1, 1, 0, 0};
     static constexpr int DY[NUM_OF_DIRECTIONS] = {0, 0, -1, 1};
@@ -275,6 +295,40 @@ std::vector<std::pair<int, int>> Master::findPath(int src_x, int src_y, int dst_
                   { delete nodeptr; });
 
     return path;
+}
+
+std::vector<std::pair<int, int>> Master::findBerthPoint(int berth_x, int berth_y) const
+{
+    /*-----------------------------------------------------------------------------*/
+    static constexpr int BERTH_SIZE = 4; // 4 * 4
+    /*-----------------------------------------------------------------------------*/
+    std::vector<std::pair<int, int>> vec_p;
+
+    // berth above
+    if (berth_x)
+        for (int i = 0; i < BERTH_SIZE; ++i)
+            if (map[berth_x - 1][berth_y + i] == PATHWAY_SYMBOL)
+                vec_p.push_back(std::make_pair(berth_x, berth_y + i));
+
+    // berth below
+    if (berth_x + 1 != N)
+        for (int i = 0; i < BERTH_SIZE; ++i)
+            if (map[berth_x + BERTH_SIZE][berth_y + i] == PATHWAY_SYMBOL)
+                vec_p.push_back(std::make_pair(berth_x + BERTH_SIZE - 1, berth_y + i));
+
+    // berth left
+    if (berth_y)
+        for (int i = 0; i < BERTH_SIZE; ++i)
+            if (map[berth_x + i][berth_y - 1] == PATHWAY_SYMBOL)
+                vec_p.push_back(std::make_pair(berth_x + i, berth_y));
+
+    // berth right
+    if (berth_y + 1 != N)
+        for (int i = 0; i < BERTH_SIZE; ++i)
+            if (map[berth_x + i][berth_y + BERTH_SIZE] == PATHWAY_SYMBOL)
+                vec_p.push_back(std::make_pair(berth_x + i, berth_y + BERTH_SIZE - 1));
+
+    return vec_p;
 }
 
 std::vector<int> Master::path2Directions(const std::vector<std::pair<int, int>> &path)
