8d5834602927ecd9a8db554f510e0d55e4d0d12b
Zhihui Gao <2049878697@qq.com>
2024-03-15 21:31:50 +0800

    Update main.cpps

--- main.cpp
+++ main.cpp
@@ -2,13 +2,5 @@
 
 int main()
 {
-    Master master;
-    master.init();
-
-    for (int frame_cnt = 0; frame_cnt < 15000; ++frame_cnt) 
-    {
-        master.update();
-        // master.control();
-        std::cout << "OK" << std::flush;
-    }
+    std::cout << sizeof(Master) << '\n';
 }
