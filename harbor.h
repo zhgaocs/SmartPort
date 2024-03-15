25d844cbdeb57275be8b593c6468decc657bc28f
Zhihui Gao <2049878697@qq.com>
2024-03-15 19:30:01 +0800

    Added and deleted some member variables

--- harbor.h
+++ harbor.h
@@ -1,33 +1,42 @@
 #ifndef HARBOR_H
 #define HARBOR_H 1
 
+#include <vector>
+
 struct Robot
 {
     int x, y;
     int has_item; /* 0 - no items, 1 - has items */
     int status;   /* 0 - recover, 1 - running */
+    /*-------------------------------------------------------------------*/
+    int has_task = 0;      // 0 represents no tasks
+    int target_berth = -1; // -1 represents no berth selected
+    std::vector<int> path; // reverse path
+    /*-------------------------------------------------------------------*/
 };
 
 struct Berth
 {
-    int id;             /* 0 <= id < BERTH_NUM */
     int x, y;           /* upper left corner */
     int transport_time; /* 1 <=  transport_time <= 1000, transport time to virtual point*/
     int loading_speed;  /* 1 <= loading_speed <= 5, the number of items that can be loaded per frame */
+    /*-------------------------------------------------------------------*/
+    int items_count = 0; // the number of items
+    int total_value = 0; // total value of the berth
+    /*-------------------------------------------------------------------*/
 };
 
 struct Boat
 {
-    int id;              /* 0 <= id < BOAT_NUM */
-    int target_berth_id; /* -1 - virtual point */
-    int status;          /* 0 - moving, 1 - running, 2 - waiting */
-    int capacity;        /* 1 <= capacity <= 1000*/
+    int target_berth = -1; /* -1 - virtual point */
+    int status = 1;        /* 0 - moving, 1 - running, 2 - waiting */
+    int capacity;          /* 1 <= capacity <= 1000*/
 };
 
 struct Item /* goods */
 {
     int x, y;
-    int value; /* value <= 1000 */
+    int value;     /* value <= 1000 */
     int life_span; /* life_span <= 1000 */
 };
 
