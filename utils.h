616ea1c3671d9cc236f40f6f3a9c8c0fc29d728e
zhgaocs <2049878697@qq.com>
2024-03-20 18:48:42 +0800

    Add extern declaration

--- utils.h
+++ utils.h
@@ -1,14 +1,19 @@
 #ifndef UTILS_H
 #define UTILS_H 1
 
-#include <stdio.h>
 #include <cmath>
 #include <cstring>
+#include <stdio.h>
+#include <unordered_map>
 #include <utility>
 #include <vector>
 #include "constants.h"
+#include "harbor.h"
 
 extern char map[N][N];
+extern std::unordered_map<int, int> nearest_berth; // key - hash(point), value - berth index
+extern std::unordered_map<int, int> point_pathidx; // key - hash(point), value - cached_paths' index
+extern std::vector<int> cached_paths[CACHED_PATH_MAX_SIZE];
 
 inline unsigned int Manhattan(int x1, int y1, int x2, int y2)
 {
